1303b60230e1fd87dba073c5fe6c7a8efd1c99df
yz <chulove522@hotmail.com.tw>
2022-04-25 05:02:32 +0800

    基礎會員系統(註冊.登入.登出.資料庫方法模組)
    基礎會員系統(註冊.登入.登出.資料庫方法模組)
    安全措施=>不能在登出狀況下回到會員
    註冊=>跟資料庫比對，不能註冊相同帳號
    會員頁面=>簡單的顯示
    資料庫方法模組=>寫一些模組之後可以用

--- Flask/main.py
+++ Flask/main.py
@@ -1,23 +1,30 @@
 import random
-from unicodedata import name
+import unicodedata
 from flask import Flask
 from flask import request
-from flask import redirect
+from flask import redirect #直接定向，目前用不到
+from flask import session
 from flask import render_template
 from matplotlib.pyplot import text
+import mongomember as mon
 import json
 #靜態路由設定
 app = Flask(__name__,
 static_folder="homepage", #資料夾名稱 在裡面的都會對應至路徑
-static_url_path="/",
+static_url_path="/",      #對應的網址路徑 "/"
 template_folder= 'templates',
-) #對應的網址路徑 "/"
+)
+#key設定
+app.secret_key = "the key"
 
-
+#狀態設定
+__islogin__ = False
+#資料庫連線
+mon.init()
 #動態路由設定#
 #內部網頁導向區#
 #首頁
-@app.route("/") #函式裝飾 設定路由 /對應的處理 
+@app.route("/") #GET方法 函式裝飾 設定路由 /對應的處理 
 def index():
     # print("請求方法", request.method)
     # print("通訊協定",request.scheme)
@@ -27,23 +34,114 @@ def index():
     # print("瀏覽器與os",request.headers.get("user-agent"))
     # print("語言偏好",request.headers.get("accept-language"))
     # print("引薦網址",request.headers.get("referrer")) #從哪連過來
+    if(__islogin__):
+        nickname_ = session["nickname"]
+        username_ = session["username","123456"]
+        return render_template("index.html",name=nickname_,user=username_)
+
     lang = request.headers.get("accept-language") #瀏覽器的偏好語言
     if(lang.startswith("zh-TW")):
         print("語言偏好:繁中")
-        return render_template("originalindex.html",name="桃桃")
+        return render_template("index.html",name="陌生人")
     else:
         print("語言偏好:英文or其他")
-        return render_template("hello")
+        return render_template("hello.html")
         #return redirect("hello")
 
+#搜尋
 @app.route("/search")
 def search():
     searchinfo = request.args.get("searchinfo","")
     return render_template("search.html",searchinfo=searchinfo)
-
-@app.route("/contact")
+#填寫聯絡表單
+@app.route("/contact", methods=["POST"])
 def contact():
-    return render_template("contact.html",name="桃桃")
+    nickname = request.form.get("nickname")
+    useremail = request.form.get("email")
+    usermessage = request.form.get("message")
+
+    return render_template("contactok.html",name=nickname,email=useremail,msg=usermessage)
+
+#註冊
+@app.route("/register") 
+def register():
+    return render_template("register.html")
+@app.route("/registerok", methods = ["POST"]) 
+def registerok():
+    username = request.form.get("username")
+    nickname = request.form.get("nickname")
+    useremail = request.form.get("email")
+    password = request.form.get("password")
+    likecategory = request.form.get("category")  #喜愛的類別 <= 這個之後再做
+    sex = request.form["gender"]
+    if sex==1:
+        sex=0
+    else:
+        sex=1
+    copy = request.form.get("copy") or""
+    human= request.form.get("human") or""
+    if human=="":
+        return render_template("error.html",errormsg=errordict[4]) #不是人
+
+    if mon.newamember(username,password,nickname,useremail,0,sex,1):
+        return render_template("registerok.html")
+    else:
+        return render_template("error.html",errormsg=errordict[3]) #被註冊了
+#登入
+@app.route("/login") 
+def login():
+    return render_template("login.html")
+@app.route("/loginok", methods = ["POST"]) 
+def loginok():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    human= request.form.get("human") or""
+    if human=="":
+        return render_template("error.html",errormsg=errordict[4]) #不是人
+#登入成功
+    if mon.login(username,password):
+        result = mon.getadatabyusername(username)
+        session["nickname"]=result["nickname"]
+        session["useremail"] = result["useremail"]
+        session["username"] = result["username"]
+        #r="/personal"
+        #?username="+username
+        return redirect("/personal")
+    else:
+        print("登入失敗")
+        return render_template("error.html",errormsg=errordict[0]) #沒帳號
+#個人頁
+@app.route("/personal")
+def personal():
+    usermessage_ ="歡迎~"
+    if "nickname" in session:
+        nickname_=session["nickname"]
+        username_=session["username"]
+        useremail_=session["useremail"]
+        return render_template("personal.html",name=nickname_,username=username_,email=useremail_,msg=usermessage_)
+    else:
+        #非法請求，直接導回首頁
+        return redirect("/")
+#錯誤頁面
+errordict = {0:"沒這個帳號",1:"密碼錯了",2:"未知錯誤發生",3:"已經有此帳號存在，請勿重複註冊",4:"未勾選您不是機器人"}
+@app.route("/error")
+def error():
+    print("產生錯誤")
+    if errnum == None:
+        errnum = request.args.get("errornumber",2)
+    errmsg = errordict.get(errnum)
+    if(errmsg==None): errmsg="這是一個還沒有被定義的錯誤"
+    return render_template("error.html",errormsg = errmsg)
+#登出
+@app.route("/signout")
+def signout():
+    #移除session，安全措施
+    if(session.__getitem__) != None:
+        del session["nickname"]
+        del session["username"]
+        del session["useremail"]
+    return redirect("/")
+
 #暫時放置區
 class TempArea:
     @app.route("/getid")
@@ -54,19 +152,17 @@ class TempArea:
         # temp =  random.randint(1,100)
         # return "隨機產生num: " + str(temp)
 
+    userlist = ('chloe','aaron','simo','meistu','better')
+
     @app.route("/user/<name>") #名字參數
     def getUser(name):
-        if name not in userlist:
-            return "you are not a member yet. please register your account"
+        if name not in __name__.userlist:
+            return render_template("register.html")
         return name
 
-    @app.route("/data/<dataname>") #資料
-    def getData(dataname):
-        if dataname not in userlist:
-            return "you are not a member yet. please register your account"
-        return dataname
 
-    userlist = ('chloe','aaron','simo','meistu','better')
+
+    
 
 #啟動
 if __name__ == "__main__":
---
+++ Flask/mongomember.py
@@ -0,0 +1,155 @@
+from asyncio.windows_events import NULL
+import random, string
+from matplotlib import collections
+from matplotlib.collections import Collection
+from numpy import sort
+import pymongo
+from urllib.parse import quote_plus
+from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient
+from bson.objectid import ObjectId
+from flask import render_template
+#----------------基礎設置--------------#
+account = __password = client = memberdb = usercollection = NULL
+#連線到資料庫
+def init():
+    global account,__password,client,memberdb,usercollection
+    account= quote_plus("peach")
+    __password=  quote_plus("3x:rgyAXQ7BWiQA")
+    client = pymongo.MongoClient("mongodb+srv://"+ account + ":" + __password + "@mycluster.5y8sq.mongodb.net/MembersData?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    #選擇資料庫memberdb
+    memberdb = client.members
+    #選擇集合users
+    usercollection = memberdb.users
+    print("已連線資料庫")
+init()
+print(usercollection)
+#資料放入集合中
+#----------------單一方法--------------#
+#找一筆資料
+def getadatabyid(id):
+    objid = id
+    userdata = usercollection.find_one(ObjectId(objid))
+    print("username:",userdata["username"])
+def getaexistbyusername(username):
+    result = usercollection.find_one({"username":username})
+    if result!= None:
+        print("帳號已存在")
+        return True #帳號已存在
+    else: return False
+#回傳一些東西
+def getadatabyusername(uname):
+    result = usercollection.find_one({"username":uname})
+    return result
+#新增一筆會員
+def newamember(username,password,nick,useremail,age,gender,level):
+    if not getaexistbyusername(username):
+        if gender==0:
+            gender ="male"
+        elif gender==1:
+            gender ="female"
+        elif gender=="male" or gender=="female":pass
+        else:print("性別不正確")
+        if level==None:
+            level=1
+        result = usercollection.insert_one({
+            "username":username,
+            "password":password,
+            "nickname":nick,
+            "useremail":useremail,
+            "age":age,
+            "gender":gender,
+            "level":level
+        })
+        print("資料新增成功")
+        #print(result.inserted_id)
+        return True
+    else:
+        return False
+
+#newamember("admin02","super","芒果","test@mail","25",1,100)
+
+#所有資料
+def getalldata():
+    cursor = usercollection.find()
+    for data in cursor:
+        print("%s等級是%s"%(data["nickname"], data["level"]))
+
+#更新單筆資料
+def setupdatedata(username,num,newdata):
+    if num==1: #更password
+        result = usercollection.update_one({"username":username},
+        {"$set":{"password":newdata}
+        })
+    elif num==2: #更暱稱
+        result = usercollection.update_one({"username":username},
+        {"$set":{"nickname":newdata}
+        })
+        #說明
+        #"$set" = 直接覆蓋 "$inc" = 加值"level":2 "$mul" = 乘值 "$unset":{"name"} 清除欄位 "$and":[]/ "$or":[] 多重條件篩選
+        print("有%d筆資料符合需求"%result.matched_count)
+        print("已完成%d筆資料的更新"%result.modified_count)
+    else:
+        print("需求不正確")
+def setlevelup(username):
+    lvup = usercollection.update_one({"username":username},
+    {
+        "$inc":{
+            "level":1
+        }
+    })
+    lv = usercollection.find_one({"username":username})
+    print("%s已經升級,%d筆資料異動,目前等級是:"%(username,lvup.modified_count),lv["level"])
+
+def setdelete_onedata(username):
+    result = usercollection.delete_one({"username":username})
+    print("刪了:",result.deleted_count,"筆")
+#異動多筆資料
+def setdelete_manydatas(level):
+    lv = int(level)
+    result = usercollection.delete_many({"level":lv})
+    print("刪了:",result.deleted_count,"筆")
+
+#getadata("6264159f953e381ee6b88f2e")
+#getalldata()
+#setupdatedata("admin",2,"可愛的管理員")
+#setlevelup("peach")
+#setdelete_onedata("hi")
+#setdelete_manydatas(3)
+#----------------------------------------#
+#隨便製造一些用戶
+def randomamenber():
+    for i in range(1,11):
+        raneng = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+        ranch =random.randint(0x4e00, 0x9FA5)
+        ranlv =random.randint(1,10)
+        ranage=random.randint(18,30)
+        rangen = random.randint(0,1)
+        newamember(raneng,"super",chr(ranch),"",ranage,rangen,ranlv)
+#randomamenber()
+
+#排序篩選
+def setlistallsort():
+    cursor = usercollection.find({},sort=[ ("level", pymongo.ASCENDING) ] )
+    for i in cursor:print(i["nickname"],"level",i["level"])
+#setlistallsort()
+
+#複合篩選
+def login(username,password):
+    if usercollection.find_one({
+        "$and":[
+            {"username":username},
+            {"password":password},
+        ]}):
+            return True
+    else:
+        return False
+#login("peach","super")
+#篩選結果
+def setresult():
+    cursor = usercollection.find({
+        "$or":[{"level":2},{"username":"admin"}]
+    } , sort=[("level",pymongo.ASCENDING)])
+    for c in cursor:
+        print(c)
+#setresult()
